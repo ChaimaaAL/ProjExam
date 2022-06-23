4be3bf07a114465bcf93391cfd3a97765283d595
ChaimaaAL <aitlarbi.chaimaa@gmail.com>
2022-06-23 22:50:52 +0100

    ProjEx

--- src/test/java/com/app/restaurant/RestaurantApplicationTests.java
+++ src/test/java/com/app/restaurant/RestaurantApplicationTests.java
@@ -1,4 +1,4 @@
-package com.app.restaurantgit;
+package com.app.restaurant;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class RestaurantgitApplicationTests {
+public class RestaurantApplicationTests {
 
     @Test
     public void contextLoads() {
